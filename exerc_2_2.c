a0457dc9c95dee3247842e6e2166a7c360d6aa90
AseelNaji <gusnajias@student.gu.se>
2017-01-31 23:47:43 +0100

    Tasks 2.2 and 2.4

---
+++ exerc_2_2.c
@@ -0,0 +1,45 @@
+/* ====================================
+File name: exerc_2_2.c (or cpp) Date: 2017-01-31
+Group Number: 5
+Members that contributed: Aseel Naji
+YU Jet Hua 
+Johan Johansson
+ * Demonstration code: [<Ass code 1-4> <abc>] 
+====================================== */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 10
+
+int main(int argc, char **argv)
+{
+    srand(time(NULL));  //generator for random numbers
+    int array[MAX];
+    int i;
+    int size = sizeof(array) / sizeof(array[0]); //getting the length of the array 
+    int *pointer;
+    int integerType;
+    pointer = &array;
+    for (i=0; i<size; i++) 
+    {    
+        array[i] = rand() %100; //range to 100
+    }
+    
+	printf("The value of the label array (address) is: %d\n", pointer);
+    printf("First integer in the array is: %d\n", array[0]);
+    printf("The size of an integer (number of bytes) is: %d\n",sizeof(integerType));
+    printf("The size of the whole array is : %d\n", sizeof(array));
+    for (i=0; i<size; i++) 
+    {    
+        printf("Elements: %d\n", array[i]);
+    }
+    pointer=array;
+    for (i=0; i<size; i++){
+        printf("Double the elements: %d\n", *pointer *2 );
+        pointer++;
+    }
+    
+	return 0;
+}
---
+++ exerc_2_4.c
@@ -0,0 +1,42 @@
+/* ====================================
+File name: exerc_2_2.c (or cpp) Date: 2017-01-31
+Group Number: 5
+Members that contributed: Aseel Naji
+YU Jet Hua 
+Johan Johansson
+ * Demonstration code: [<Ass code 1-4> <abc>] 
+====================================== */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    char string[100];
+    int leftLetter, rightLetter, length = 0;
+    printf("Enter a word\n");
+    scanf("%s", string);
+    /* Find length of input string */
+    while(string[length] != '\0')
+        length++;
+    /* If length of string is less than 1, ERROR */
+    if(length < 1) 
+        return 1;
+         
+    /* Initialize leftLetter and rightLetter to first and 
+     last character of input string */
+    leftLetter = 0;
+    rightLetter = length -1;
+    /* Compare left and right characters, If equal then 
+     continue otherwise not a palindrome */
+    while(leftLetter < rightLetter){
+        if(string[leftLetter] != string[rightLetter]){
+            printf("%s is not a Palindrome \n", string);
+            return 0;
+        }
+        leftLetter++;
+        rightLetter--;
+    }
+    printf("%s is a Palindrome \n", string);
+    return 0;
+}
+
