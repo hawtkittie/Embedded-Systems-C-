3b58d929528eb4b3fb0eaf28b6ac7663382b10f0
Blondiejohan <johan.johansson.swe@gmail.com>
2017-02-01 21:49:04 +0100

    Add files via upload

--- exerc_2_5.c
+++ exerc_2_5.c
@@ -4,8 +4,6 @@ Date: 2017-01-30
         Group Number:
         Members that contributed:
 Johan Johansson
- Yu Jet Hua
-Aseel Naji
 
 Demonstration code: [<Ass code 1-4> <abc>] Important , No code no bonus !
 ====================================== */
@@ -14,54 +12,55 @@ Demonstration code: [<Ass code 1-4> <abc>] Important , No code no bonus !
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX 100
+#define MAX 10
 #define MAXNUMBER 20
 
 
-void create_random( int tab[] );
-void count_frequency(int tab[], int freq[] );
-void draw_histogram(int freq[] );
+void create_random( int *tab );
+void count_frequency(int *tab, int *freq );
+void draw_histogram(int *freq );
 
 int main() {
     int tab[MAX],n;
-    int freq[MAXNUMBER];
+    int freq[MAXNUMBER]={0};
+    int *tabP = tab;
+    int *freqP = freq;
 
 
-    create_random(tab);
-
-    count_frequency(tab,freq);
-
-    draw_histogram(freq);
+    create_random(tabP);
+    count_frequency(tabP,freqP);
+    draw_histogram(freqP); 
 }
 
-void create_random(int tab[]){
+void create_random(int *tab){
     srand(time(NULL));
     for (int i = 0; i < MAX; i++) {
         int random = rand() % MAXNUMBER;
-        tab[i] = random;
-        //printf("%i %i \n",i,tab[i]);
+        *tab = random;
+        printf("%i ",*tab);
+        tab++;
     }
 }
 
-void count_frequency(int tab[], int freq[] ){
-    for (int j = 0; j < MAXNUMBER ; ++j) {
-        freq[j]=0;
-    }
+void count_frequency(int *tab, int *freq ){
     for (int i = 0; i < MAX; i++) {
-        //printf("%i ",tab[i]);
-        freq[tab[i]]++;
+        freq=freq+*tab;
+        *freq=*freq+1;
+        freq=freq-*tab;
+        tab++;
     }
 }
 
-void draw_histogram(int freq[] ){
+void draw_histogram(int *freq ){
     for (int i = 0; i < MAXNUMBER; i++) {
-        if(freq[i] != 0){
+        if(*freq != 0){
             printf("\n %i ",i);
-            while(freq[i] != 0){
+            while(*freq!= 0){
                 printf("x");
-                freq[i]--;
+                *freq=*freq-1;
             }
         }
+        freq++;
     }
 }
 
